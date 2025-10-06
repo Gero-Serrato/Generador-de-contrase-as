e2217df1620c5419edadfb24abcfb57fea57e67b
Gero-Serrato <geronimoserratom@gmail.com>
2025-10-06 18:08:28 -0500

    Update Generador_Contraseñas.py

--- Proyecto 1/Generador_Contraseñas.py
+++ Proyecto 1/Generador_Contraseñas.py
@@ -1,7 +1,54 @@
+import discord
+
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run("TOKEN")
+
+
+
+
+
 import random
-Caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-Longitud_contraseña = input("Ingrese la longitud deseada de la contraseña: ")
-Contraseña = ""
-for i in range(Longitud_contraseña):
-    Contraseña += random.choice(Caracteres)
-print("tu contraseña es: " (Contraseña))
+
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "HEADS"
+    else:
+        return "TAILS"
